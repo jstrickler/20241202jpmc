17a4c55c6c9818b536aac9ab168b207ef586d9d7
John Strickler <jstrickler@gmail.com>
2024-12-05 18:14:36 +0000

    end of class

--- EXAMPLES/flask_examples/flask_hello.py
+++ EXAMPLES/flask_examples/flask_hello.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)  # create Flask application object
-
 
 @app.route('/')  # assign this view function to '/' (i.e., the "root: of the web site)
 def index():  # define the view function
     return '<h1>Hello, Flask world!</h1>' # return HTML for the page to display
+
+@app.route('/login')
+def login():
+    return Response('<h3>Login here...</h3>', status=200, mimetype="text/html")
+
+@app.route('/api/wombat/<int:id>')
+def wombat(id):
+    return Response("{" + f'"id": "{id}"' +  "}", mimetype="application/json")
 
 # app.register_route(index, '/')
 
